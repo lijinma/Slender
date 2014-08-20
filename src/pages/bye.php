5794730a5e75ec18be2f57b2659d56ae2760da79
Jinma li <lijinma@126.com>
2014-08-20 14:51:31 +0800

    - Lesson 3 http://fabien.potencier.org/article/52/create-your-own-framework-on-top-of-the-symfony2-components-part-3

---
+++ src/pages/bye.php
@@ -0,0 +1,2 @@
+<!-- /src/pages/hello.php -->
+Goodbye!
---
+++ src/pages/hello.php
@@ -0,0 +1,5 @@
+<!-- /src/pages/hello.php -->
+ 
+<?php $name = $request->get('name', 'World') ?>
+ 
+Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
